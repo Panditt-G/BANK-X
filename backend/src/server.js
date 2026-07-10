89610dc65ddf4960782957f884e22be841bfe7fa
pandit-g <panditvishal2006@gmail.com>
2026-07-10 19:56:38 +0530

    url fix for front end

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,7 @@ app.get("*name",(req,res)=>{
 })
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
